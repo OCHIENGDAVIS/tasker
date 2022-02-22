3e1d35415a28bc3faca691e049af85bcc278bdb8
davis <davisochieng4@gmail.com>
2022-02-22 17:01:50 -0500

    Added request body validation using express-validater package on /personnel/register and /personnel/login routes

--- server/routes/personel.js
+++ server/routes/personel.js
@@ -1,13 +1,36 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-  return res.json({ mesage: 'Hello there' });
-});
+router.post(
+  '/register',
+  body('email').not().isEmpty().trim().escape(),
+  body('email').isEmail(),
+  body('firstname').not().isEmpty().trim().escape(),
+  body('lastname').not().isEmpty().trim().escape(),
+  body('password').not().isEmpty().trim().escape(),
+  body('phone').not().isEmpty().trim().escape(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ erros: errors.array() });
+    }
+    return res.json({ mesage: 'Hello there' });
+  }
+);
 
-router.post('/login', (req, res) => {
-  return res.json({ message: 'Post route' });
-});
+router.post(
+  '/login',
+  body('phone').not().isEmpty().trim().escape(),
+  body('password').not().isEmpty().trim().escape(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ erros: errors.array() });
+    }
+    return res.json({ message: 'Post route' });
+  }
+);
 
 module.exports = router;
